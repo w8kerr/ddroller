356f17a6d40f12baaca7a7b0abdc29fd5e88b9de
w8kerr <36288420+w8kerr@users.noreply.github.com>
2018-02-09 15:49:12 -0500

    Refactoring
    Add error codes
    Partition logic into smaller functions
    Use structs for passing data

--- main.go
+++ main.go
@@ -6,8 +6,54 @@ import (
   "regexp"
   "strconv"
   "math/rand"
+  "time"
 )
 
+//Error values for parsing roll requests.
+const (
+  RollError_UnsupportedFormat = iota
+  RollError_UnsupportedDice
+  RollError_RequestTooLarge
+)
+
+//Don't roll more than this many dice at a time
+const DiceCountLimit int = 1000
+
+//Only roll dice with predefined number of sides
+var SupportedDice = map[int]bool {
+  2: true,
+  4: true,
+  6: true,
+  8: true,
+  10: true,
+  20: true,
+  100: true,
+}
+
+//Definition for a roll request.
+//Roll [count] dice with [sides], add [modified],
+//and succeed if the result is above [success],
+//or below [success] if [reverse_success] is true.
+type RollDef struct {
+  count int
+  sides int
+  modifier int
+  success int
+  reverse_success bool
+}
+
+//Definition for a roll result.
+//[rolls] contains each individual roll, [total] contains the sum including the
+//roll modifier, and [succeeded] is true if the roll met its success threshold.
+type RollResult struct {
+  rolls []int
+  total int
+  succeeded bool
+}
+
+func init() {
+  rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
   scanner := bufio.NewReader(os.Stdin)
@@ -16,29 +62,86 @@ func main() {
   fmt.Print("Roll: ")
   roll_request, _ = scanner.ReadString('\n')
 
-  roll_matcher, _ := regexp.Compile(`(\d+)d(\d+)`)
+  roll_def, err := ParseRoll(roll_request)
+  if err != -1 {
+    DisplayParseError(err, roll_def)
 
-  if roll_matcher.MatchString(roll_request) {
-    parsed_roll_request := roll_matcher.FindStringSubmatch(roll_request)
-    fmt.Println("Roll request: ", roll_request)
-    num_dice, _ := strconv.Atoi(parsed_roll_request[1])
-    dice_sides, _ := strconv.Atoi(parsed_roll_request[2])
-    fmt.Println("Thats ", num_dice, " dice with ", dice_sides, " sides.")
-
-    rand.Seed(20)
-    var roll_total int
-    for i := 1; i <= num_dice; i++ {
-      one_roll := rand.Intn(dice_sides) + 1
-      fmt.Println("Roll ", i, ": ", one_roll)
-      roll_total += one_roll
-    }
-
-    fmt.Println("\nTotal: ", roll_total)
-  } else {
-    fmt.Println(`Your request was not in valid DnD roll syntax.
-Use {number of dice}d{numbers of sides per dice}
-For example: '3d20' rolls three 20-sided dice.`)
+    //Program cannot continue after parsing error, so stop execution
+    scanner.ReadString('\n')
+    return
   }
 
+  roll_result := PerformRoll(roll_def)
+
+  for i, roll := range roll_result.rolls {
+    fmt.Println("Roll ", i+1, ": ", roll)
+  }
+  fmt.Println("\nTotal: ", roll_result.total)
+
   scanner.ReadString('\n')
+  return
+}
+
+func ParseRoll(request string) (def RollDef, err int) {
+  roll_matcher, _ := regexp.Compile(`(?P<count>\d+)d(?P<sides>\d+)`)
+
+  if !roll_matcher.MatchString(request) {
+    return def, RollError_UnsupportedFormat
+  } else {
+    err = -1
+    parsed_request := roll_matcher.FindStringSubmatch(request)
+    count, _ := strconv.Atoi(parsed_request[1])
+    sides, _ := strconv.Atoi(parsed_request[2])
+
+    def.count = count
+    if count > DiceCountLimit {
+      err = RollError_RequestTooLarge
+    }
+
+    def.sides = sides
+    if !SupportedDice[sides] {
+      err = RollError_UnsupportedDice
+    }
+
+    def.modifier = 0
+    def.success = 0
+
+    return def, err
+  }
+}
+
+func PerformRoll(def RollDef) RollResult {
+  var res RollResult
+  res.rolls = make([]int, 0, def.count)
+
+  var roll_total int
+  for i := 1; i <= def.count; i++ {
+    one_roll := rand.Intn(def.sides) + 1
+    res.rolls = append(res.rolls, one_roll)
+    roll_total += one_roll
+  }
+
+  res.total = roll_total + def.modifier
+  if def.reverse_success {
+    //Reversed success: succeed if the total is <= the threshold
+    res.succeeded = res.total <= def.success
+  } else {
+    //Normal success: succeed if the total is >= the threshold
+    res.succeeded = res.total >- def.success
+  }
+
+  return res
+}
+
+func DisplayParseError(err int, def RollDef) {
+  switch err {
+    case RollError_UnsupportedFormat:
+      fmt.Println("Your request was not in valid DnD roll syntax.\n" +
+      "Format your request in the style of 2d20,\n" +
+      "which rolls two dice with 20 sides each.")
+    case RollError_RequestTooLarge:
+      fmt.Println("Cannot roll more than ", DiceCountLimit, " dice.")
+    case RollError_UnsupportedDice:
+      fmt.Println("Cannot roll dice with ", def.sides, " sides.")
+  }
 }
